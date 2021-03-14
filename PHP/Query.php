40abdb57faf4ea88bd0784f85f6469df7fa9b07f
Alexandra Sirois <asirois9191@gmail.com>
2021-03-13 20:34:37 -0500

    add qery

---
+++ PHP/Query.php
@@ -0,0 +1,15 @@
+<?php
+require_once('ConnectionDB.php');
+
+class Query
+{
+    private $connexion;
+
+    public function __construct()
+    {
+        $constants = new ConnectionDB();
+        $this->connexion = $constants->getConnexion();
+    }
+
+
+}
